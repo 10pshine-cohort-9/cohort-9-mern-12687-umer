670fb30a1f4ca28e71d20d349c2d68d3f70adfa9
Umer <safee4600033@cloud.neduet.edu.pk>
2026-07-23 00:03:06 +0500

    ESM compatible paths in backend/prisma.config

--- backend/prisma/prisma.config.ts
+++ backend/prisma/prisma.config.ts
@@ -1,6 +1,11 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import { config } from "dotenv";
 import { defineConfig, env } from "prisma/config";
+
+// Recreate __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Force dotenv to step up into the project root directory
 config({ path: path.resolve(__dirname, "../.env") });
